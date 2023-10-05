709502a93b94e7be6f489b910b2d7c2ff9a0b5a6
Oleksii <110523568+Oleksii-co@users.noreply.github.com>
2023-10-05 20:11:09 +0300

    done 1-6

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,11 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
+let literal: "enable" | "disable";
+
+union = "qwerty";
+union = 123456;
+
+literal = "enable";
+literal = "disable";
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,9 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+const enum Days {
+
+}
+
+// function (isWeekend:string)
